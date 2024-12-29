fdce08321dabb58e5ae2c0f7aeb37f0cb61bb39b
Galih. <galihpujiirianto@gmail.com>
2024-12-29 08:44:11 +0700

    Initial commit

---
+++ resources/js/Pages/Index.jsx
@@ -0,0 +1,10 @@
+import { Head } from "@inertiajs/react";
+
+export default function Index() {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <Head title="Hello World!" />
+            <h1 className="text-3xl font-bold">Hello World!</h1>
+        </div>
+    );
+}
